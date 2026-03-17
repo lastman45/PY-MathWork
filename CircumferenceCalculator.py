d300ff88d4bc345b09e0eb9fca1d985a486c1ac2
okey8liver <167875491+lastman45@users.noreply.github.com>
2026-03-17 09:49:47 +0300

    Add files via upload

---
+++ CircumferenceCalculator.py
@@ -0,0 +1,5 @@
+import math
+
+radius = float(input("Enter the radius of a circle(cm): "))
+circumference = 2 * math.pi * radius
+print(f"The circumference of the circle is: {round(circumference, 2)}cm")
